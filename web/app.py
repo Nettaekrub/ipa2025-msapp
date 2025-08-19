fe68b2e97ab8de30c4bc42d0ea0968667ecc32f5
Nettaekrub <Undaaa12345@gmail.com>
2025-08-19 10:14:03 +0000

    k

--- web/app.py
+++ web/app.py
@@ -4,10 +4,16 @@ from flask import render_template
 from flask import redirect
 from flask import url_for
 from pymongo import MongoClient
+from bson import ObjectId
+import os
 
 app = Flask(__name__)
-client = MongoClient("mongodb://mongo:27017/")
-db = client["mydb"]                             
+
+mongo_uri  = os.environ.get("MONGO_URI")
+db_name    = os.environ.get("DB_NAME")
+
+client = MongoClient(mongo_uri)
+db = client[db_name]                             
 routers = db["routers"]
 
 @app.route("/")
